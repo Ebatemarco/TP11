1fa3dc705eb104c03642171a0e68b50693d38674
Ebatemarco <72512561+Ebatemarco@users.noreply.github.com>
2020-10-29 16:08:13 -0300

    Add files via upload

---
+++ TP11Printf/MainPrint.c
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   MainPrint.c
+ * Author: ebatemarco
+ *
+ * Created on 29 de octubre de 2020, 15:57
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    
+    return (EXIT_SUCCESS);
+}
+
+void mi_printf(char* string, ...)
+{
+    va_list param_list;
+    va_start (param_list, string); 
+    
+    while(string)
+    
+}
+
+
